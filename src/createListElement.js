668335959f7a945cea6aa312e1d33beb8cb6433e
Martin Valov <martin.valov98@gmail.com>
2022-10-06 13:21:33 +0300

    creating div for each project instead of li elements

--- src/createListElement.js
+++ src/createListElement.js
@@ -1,15 +1,20 @@
 import getRepos from "./getRepos.js";
 
 function createListElement(repo) {
-    const liEl = document.createElement('li');
-    liEl.innerHTML = 
-        `<div class="project-tile">
-            <h5 class="project-title">${repo.full_name}</h5>
-            <a href="${repo.html_url}" target="_blank">Open in GitHub</a>
-        </div>`;
+    const projectTile = document.createElement('div');
+    const projectTitle = document.createElement('h5');
+    const projectLink = document.createElement('a');
+
+    projectTile.classList.add('project-tile');
+    projectTitle.classList.add('project-title');
+    projectTitle.textContent = repo.full_name;
+    projectLink.href = repo.full_name;
+    projectLink.textContent = 'Open in GitHub';
     
+    projectTile.appendChild(projectTitle);
+    projectTile.appendChild(projectLink);
     
-    return liEl;
+    return projectTile;
 }
 
 async function loadRepos() {
