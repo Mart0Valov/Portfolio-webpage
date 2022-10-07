0279c7c891ec41790dd4f0adba06333fe48f7b59
Martin Valov <martin.valov98@gmail.com>
2022-10-07 13:23:11 +0300

    adding target for link card element

--- src/createListElement.js
+++ src/createListElement.js
@@ -3,8 +3,9 @@ import getRepos from "./getRepos.js";
 function createListElement(repo) {
     const projectLink = document.createElement('a');
     const projectName = repo.full_name.split('/')[1];
-    projectLink.href = repo.html_url;
     projectLink.classList.add('project-card');
+    projectLink.href = repo.html_url;
+    projectLink.target = '_blank';
     projectLink.textContent = projectName;
     
     return projectLink;
